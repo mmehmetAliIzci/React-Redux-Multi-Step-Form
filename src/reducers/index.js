fa81cc8bbcffbaa45c4a20c3030f31c255826db8
MEHMET ALI IZCI <mmehmetaliizci@hotmail.com>
2017-12-07 16:19:03 +0100

    Combine tabHolder Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import fuelSavings from './fuelSavingsReducer';
+import tabHolder from './tabHolderReducer';
 import { routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({
   fuelSavings,
+  tabHolder,
   routing: routerReducer
 });
 

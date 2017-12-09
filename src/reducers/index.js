16b8c46aabe2b9330eb7238a838077f933e3665a
MEHMET ALI IZCI <mmehmetaliizci@hotmail.com>
2017-12-09 21:47:26 +0100

    Add tab4 actions/reducers/ action Types / Forgotten tab3 combiner

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,8 @@ import fuelSavings from './fuelSavingsReducer';
 import tabHolder from './tabHolderReducer';
 import tab1 from './tab1Reducer';
 import tab2 from './tab2Reducer';
+import tab3 from './tab3Reducer';
+import tab4 from './tab4Reducer';
 import { routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({

8c3bbe2bae29012222fe46e67e72ed26878483cc
MEHMET ALI IZCI <mmehmetaliizci@hotmail.com>
2017-12-09 21:51:15 +0100

    Add tab3 actions, reducers

---
+++ src/actions/tab3Actions.js
@@ -0,0 +1,41 @@
+import * as types from '../constants/actionTypes';
+
+import { checkIt } from './mockedApi'
+
+
+
+// example of a thunk using the redux-thunk middleware
+export function sendText(payloads) {
+  return function (dispatch) {
+    return dispatch({
+      type: types.SEND_TEXT,
+      payload: checkIt(payloads)
+    });
+  };
+}
+
+
+// example of a thunk using the redux-thunk middleware
+export function sendTextSuccess(savedText) {
+  return function (dispatch) {
+    // thunks allow for pre-processing actions, calling apis, and dispatching multiple actions
+    // in this case at this point we could call a service that would persist the fuel savings
+    return dispatch({
+      type: types.SEND_TEXT_SUCCESS,
+      payload: savedText
+    });
+  };
+}
+
+// example of a thunk using the redux-thunk middleware
+export function sendTextFailure(error) {
+  debugger;
+  return function (dispatch) {
+    // thunks allow for pre-processing actions, calling apis, and dispatching multiple actions
+    // in this case at this point we could call a service that would persist the fuel savings
+    return dispatch({
+      type: types.SEND_TEXT_FAIL,
+      payload: error
+    });
+  };
+}
---
+++ src/reducers/tab3Reducer.js
@@ -0,0 +1,33 @@
+import {SEND_TEXT,SEND_TEXT_SUCCESS,SEND_TEXT_FAIL} from '../constants/actionTypes';
+
+
+const INITIAL_STATE = {savedText: '', error: null, loading:null}
+
+// IMPORTANT: Note that with Redux, state should NEVER be changed.
+// State is considered immutable. Instead,
+// create a copy of the state passed and set new values on the copy.
+// Note that I'm using Object.assign to create a copy of current state
+// and update values on the copy.
+export default function tab3Reducer(state = INITIAL_STATE, action) {
+
+  switch (action.type) {
+    case SEND_TEXT:
+    debugger;
+      // For this example, just simulating a save by changing date modified.
+      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
+    return { ...state, savedText: '', error: null, loading: false };
+    case SEND_TEXT_SUCCESS:
+      // For this example, just simulating a save by changing date modified.
+      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
+    return { ...state, savedText: action.payload, loading: false };
+    case SEND_TEXT_FAIL:
+    debugger;
+      // For this example, just simulating a save by changing date modified.
+      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
+    return { ...state, error: action.payload, loading: false };
+
+    default:
+
+      return state;
+  }
+}

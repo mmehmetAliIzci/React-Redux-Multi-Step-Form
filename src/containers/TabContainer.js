ac19932a6d5890c900e2da8b391f1dc6d73bd5f8
MEHMET ALI IZCI <mmehmetaliizci@hotmail.com>
2017-12-09 22:04:51 +0100

    Change tabnumbers from string to int

--- src/containers/TabContainer.js
+++ src/containers/TabContainer.js
@@ -11,30 +11,31 @@ import Tab5 from '../components/tab5.js'
 import NotFoundPage from '../components/NotFoundPage';
 
 export class TabContainer extends React.Component {
-  changeTab = tabNumber => {
-    this.props.actions.changeTab(tabNumber);
+  changeTab = (tabNumber) => {
+    console.log(parseInt(tabNumber));
+    this.props.actions.changeTab(parseInt(tabNumber));
   }
 
   componentDidMount(){
-    this.props.actions.changeTab("1");
+    this.props.actions.changeTab(3);
   }
 
   render() {
     let template;
     switch (this.props.currentTab) {
-      case "1":
+      case 1:
         template = <Tab1 toNextTab={this.changeTab}/>;
       break;
-      case "2":
+      case 2:
         template = <Tab2 toNextTab={this.changeTab}/>;
       break;
-      case "3":
+      case 3:
         template = <Tab3 toNextTab={this.changeTab}/>;
       break;
-      case "4":
+      case 4:
         template = <Tab4 toNextTab={this.changeTab}/>;
       break;
-      case "5":
+      case 5:
         template = <Tab5 toNextTab={this.changeTab}/>;
       break;
       default:

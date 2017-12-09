3e7a9f78fc80973266df13e52556f0aaa8f9d111
MEHMET ALI IZCI <mmehmetaliizci@hotmail.com>
2017-12-10 00:21:50 +0100

    Remove unnecessary Comments

--- src/reducers/tab3Reducer.js
+++ src/reducers/tab3Reducer.js
@@ -3,31 +3,16 @@ import {SEND_TEXT,SEND_TEXT_SUCCESS,SEND_TEXT_FAIL} from '../constants/actionTyp
 
 const INITIAL_STATE = {savedText: '', error: null, loading:null}
 
-// IMPORTANT: Note that with Redux, state should NEVER be changed.
-// State is considered immutable. Instead,
-// create a copy of the state passed and set new values on the copy.
-// Note that I'm using Object.assign to create a copy of current state
-// and update values on the copy.
 export default function tab3Reducer(state = INITIAL_STATE, action) {
 
   switch (action.type) {
     case SEND_TEXT:
-
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
-    return { ...state, savedText: '', error: null, loading: true };
+      return { ...state, savedText: '', error: null, loading: true };
     case SEND_TEXT_SUCCESS:
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
-    return { ...state, savedText: action.payload, loading: false };
+      return { ...state, savedText: action.payload, loading: false };
     case SEND_TEXT_FAIL:
-
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
-    return { ...state, error: action.payload, loading: false };
-
+      return { ...state, error: action.payload, loading: false };
     default:
-
       return state;
   }
 }
--- src/reducers/tab5Reducer.js
+++ src/reducers/tab5Reducer.js
@@ -3,25 +3,14 @@ import {SEND_FORM_CONTENT,SEND_FORM_CONTENT_SUCCESS,SEND_FORM_CONTENT_FAIL} from
 
 const INITIAL_STATE = {sentContent: '', error: null, loading:null}
 
-// IMPORTANT: Note that with Redux, state should NEVER be changed.
-// State is considered immutable. Instead,
-// create a copy of the state passed and set new values on the copy.
-// Note that I'm using Object.assign to create a copy of current state
-// and update values on the copy.
 export default function tab3Reducer(state = INITIAL_STATE, action) {
 
   switch (action.type) {
     case SEND_FORM_CONTENT:
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
     return { ...state, sentContent: '', error: null, loading: true };
     case SEND_FORM_CONTENT_SUCCESS:
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
     return { ...state, sentContent: action.payload, loading: false };
     case SEND_FORM_CONTENT_FAIL:
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
     return { ...state, error: action.payload, loading: false };
 
     default:
